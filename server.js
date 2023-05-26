99f875081c9de50a19af281ab23351e70e925a20
carmencmattos <carmencmattos@gmail.com>
2023-05-26 16:57:14 -0300

    fix: remove logs

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import app from "./src/app.js";
 
 const port = process.env.PORT || 3000;
-console.log(process.env.MONGODB_CONNECT_KEY);
+
 app.listen(port, () => {
   console.log(`Servidor escutando em http://localhost:${port}`);
 });
--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-console.log(process.env.MONGODB_CONNECT_KEY);
+
 mongoose.connect(process.env.MONGODB_CONNECT_KEY);
 
 let db = mongoose.connection;
